b2648ad4226d467a33df7caa35f46d56102bb5d1
Laila Maaloum <lailamaaloum15@gmail.com>
2024-11-04 09:50:40 +0100

    Initial commit

---
+++ demo/src/main/java/LibraryItem/Journal.java
@@ -0,0 +1,19 @@
+class Journal extends LibraryItem {
+    private String field;
+
+    public Journal(String title, String author, String field) {
+        super(title, author);
+        this.field = field;
+    }
+
+    @Override
+    public String getItemType() {
+        return "Journal";
+    }
+
+    @Override
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("Field: " + field);
+    }
+}
